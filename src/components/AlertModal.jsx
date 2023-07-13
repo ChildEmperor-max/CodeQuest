2fad7d5acbbf80b1c0721a71b42bde35d4d9e4a4
Rogelio L. Haban Jr <rogeliohabanjr19@gmail.com>
2023-07-13 20:46:38 +0800

    added ok button

--- src/components/AlertModal.jsx
+++ src/components/AlertModal.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
-const AlertModal = ({ message, onConfirm, onCancel }) => {
+const AlertModal = ({
+  message,
+  onConfirm = null,
+  onCancel = null,
+  onOk = null,
+}) => {
   return (
     <div className="alert-modal-container">
       <div className="alert-modal-message">{message}</div>
       <div className="alert-modal-buttons">
-        <button onClick={onConfirm}>Yes</button>
-        <button onClick={onCancel}>No</button>
+        {!onOk ? (
+          <>
+            <button onClick={onConfirm}>Yes</button>
+            <button onClick={onCancel}>No</button>
+          </>
+        ) : (
+          <button onClick={onOk}>Ok</button>
+        )}
       </div>
     </div>
   );
