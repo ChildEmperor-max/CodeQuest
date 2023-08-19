29ea669766bb300118f291d873ee7e5a40f246b4
Rogelio L. Haban Jr <rogeliohabanjr19@gmail.com>
2023-08-19 18:29:38 +0800

    changed variable name

--- src/world/LoadWorld.js
+++ src/world/LoadWorld.js
@@ -2,16 +2,18 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import * as THREE from "three";
 
 const loader = new GLTFLoader();
+const visibilityThreshold = 100;
+let objectsToRender = [];
 
 export function LoadWorld() {
   return new Promise((resolve, reject) => {
     loader.load(
       "/src/assets/world/codequest_map.glb",
       function (gltf) {
-        const terrainMesh = gltf.scene;
-        terrainMesh.name = "terrain";
-        terrainMesh.position.set(3, 0, 3);
-        terrainMesh.scale.set(1, 1, 1);
+        const worldMesh = gltf.scene;
+        worldMesh.name = "terrain";
+        worldMesh.position.set(3, 0, 3);
+        worldMesh.scale.set(1, 1, 1);
         const obstacles = [];
         const walkables = [];
         const transferAreas = [];
@@ -30,10 +32,13 @@ export function LoadWorld() {
           npcSpawnPoint4,
         ];
 
-        terrainMesh.traverse(function (child) {
+        worldMesh.traverse(function (child) {
           if (child instanceof THREE.Mesh) {
             child.castShadow = true;
             child.receiveShadow = true;
+            if (child.name.startsWith("Sphere")) {
+              child.frustumCulled = false;
+            }
           }
           if (child.isMesh && child.name.startsWith("Walkable_")) {
             child.visible = false;
@@ -58,7 +63,7 @@ export function LoadWorld() {
         });
 
         resolve({
-          terrainMesh,
+          worldMesh,
           worldFloor,
           walkables,
           obstacles,
@@ -74,4 +79,19 @@ export function LoadWorld() {
       }
     );
   });
+}
+
+export function updateWorldRender(playerPosition) {
+  for (var object of objectsToRender) {
+    // Calculate the distance between the player and the object
+    const objectPosition = object.position; // Replace with your object's position
+    const distanceToPlayer = playerPosition.distanceTo(objectPosition);
+
+    // Check if the distance is within the visibility threshold
+    if (distanceToPlayer <= visibilityThreshold) {
+      object.visible = true; // Make the object visible
+    } else {
+      object.visible = false; // Hide the object
+    }
+  }
 }
