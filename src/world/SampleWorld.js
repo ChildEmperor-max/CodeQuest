02c3fd1bc27cdafa7fac64c310ea37fe7d02b0b1
ChildEmperor-max <84144769+ChildEmperor-max@users.noreply.github.com>
2023-08-22 13:12:11 +0800

    new commit

--- src/world/SampleWorld.js
+++ src/world/SampleWorld.js
@@ -45,6 +45,7 @@ export function LoadSampleWorld() {
             }
             if (child.name.startsWith("Floor_")) {
               worldFloor = child;
+              walkables.push(worldFloor);
             }
           }
         });
