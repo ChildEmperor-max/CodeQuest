e710f5c0dc3b3ecb554165bc1f5754022f695cef
ChildEmperor-max <84144769+ChildEmperor-max@users.noreply.github.com>
2023-09-09 16:32:54 +0800

    added 2 new column

--- server/sql/getDialogByNpcId.sql
+++ server/sql/getDialogByNpcId.sql
@@ -1,4 +1,4 @@
-SELECT dialog.id, npc.npc_name, dialog.dialog, dialog.response_to, dialog.branch
+SELECT dialog.id, npc.npc_name, dialog.dialog, dialog.response_to, dialog.branch, dialog.is_array, dialog.stage
 FROM npc
 LEFT JOIN dialog
 ON npc.id = $1
