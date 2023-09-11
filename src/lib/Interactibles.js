e70ce6a996637e05413de896a10d8f6cd6bb4dd1
Rogelio L. Haban Jr <rogeliohabanjr19@gmail.com>
2023-09-11 14:09:41 +0800

    added quest icon and modified the nested ifs

--- src/lib/Interactibles.js
+++ src/lib/Interactibles.js
@@ -7,42 +7,81 @@ export default class Interactibles extends THREE.Object3D {
   }
   initialize(position, camera, player) {
     this.dynamicLabel = new DynamicLabel();
-    this.nameDisplayYposition = 95;
-    this.interactLabelYPosition = 5;
     this.interactRange = 5;
+    this.questIconRange = 30;
     this.player = player;
     this.isInteracting = false;
     this.hasDialog = false;
+    this.hasQuest = false;
+    this.hasStoryQuest = false;
+    this.hasSideQuest = false;
+    this.questIconIsSet = false;
+    this.finishedQuestSetting = false;
+    this.camera = camera;
+    this.position.set(position.x, position.y, position.z);
 
     this.dynamicLabel.setInteractLabel({
       camera: camera,
       position: position,
-      yOffset: this.interactLabelYPosition,
+      yOffset: 120,
     });
   }
 
+  setQuestIcon() {
+    if (this.questIconIsSet && (this.hasStoryQuest || this.hasSideQuest)) {
+      const storyQuest = this.hasStoryQuest;
+      this.dynamicLabel.setQuestIcon({
+        camera: this.camera,
+        position: this.position,
+        yOffset: 200,
+        storyQuest,
+      });
+
+      this.questIconIsSet = false;
+      this.finishedQuestSetting = true;
+    }
+  }
+
   update(delta) {
-    if (this.npcNearPlayer()) {
-      if (this.hasDialog) {
-        this.dynamicLabel.showInteractLabel(this.position, this.camera);
-        this.player.onNpcZone(this);
-        if (this.player.interactingWithNpc === this.npcName) {
-          this.dynamicLabel.hideInteractLabel();
+    if (this.hasQuest) {
+      this.setQuestIcon();
+      if (this.finishedQuestSetting) {
+        if (this.npcNearPlayer(this.questIconRange)) {
+          this.dynamicLabel.showQuestIcon(this.position, this.camera);
+        } else {
+          this.dynamicLabel.hideQuestIcon();
         }
       }
+    }
+
+    if (this.npcNearPlayer(this.interactRange)) {
+      this.isInteractable();
     } else {
       this.dynamicLabel.hideInteractLabel();
-      if (this.npcName === this.player.interactingWithNpc) {
+      if (this.interactingWithPlayer()) {
         this.player.onNpcZone(null);
       }
     }
-    this.dynamicLabel.updateInteractLabel(this.position, this.camera);
   }
 
-  npcNearPlayer() {
+  isInteractable() {
+    if (this.hasDialog) {
+      this.dynamicLabel.showInteractLabel(this.position, this.camera);
+      this.player.onNpcZone(this);
+      if (this.interactingWithPlayer()) {
+        this.dynamicLabel.hideInteractLabel();
+      }
+    }
+  }
+
+  interactingWithPlayer() {
+    return this.npcName === this.player.interactingWithNpc;
+  }
+
+  npcNearPlayer(range) {
     return (
       Math.abs(this.player.getPosition().distanceTo(this.getPosition())) <=
-      this.interactRange
+      range
     );
   }
 
