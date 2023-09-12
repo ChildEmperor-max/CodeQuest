e786e76360d508ee8f26953756449cf807da03e7
Rogelio L. Haban Jr <rogeliohabanjr19@gmail.com>
2023-09-12 18:14:10 +0800

    changed from quest_title to quest_id to fetch

--- server/handlers/questHandler.js
+++ server/handlers/questHandler.js
@@ -58,48 +58,28 @@ export function handleFetchCompletedQuestCount(req, res, pool) {
 export function handleUpdateQuestStatus(req, res, pool) {
   try {
     const questData = req.body;
-    const questTitle = questData.quest_title;
+    const questId = questData.quest_id;
     const newQuestStatus = questData.quest_status;
-    pool.query(
-      "SELECT id FROM quest WHERE quest_title = $1",
-      [questTitle],
-      (err, result) => {
-        if (err) {
-          console.error("Error checking Quest ID:", err);
-          res.writeHead(500, { "Content-Type": "application/json" });
-          res.end(JSON.stringify({ error: "Internal Server Error" }));
-          return;
-        }
-        if (result.rows.length === 0) {
-          res.writeHead(404, { "Content-Type": "application/json" });
-          res.end(JSON.stringify({ error: "Quest not found" }));
-          return;
-        }
-        const questId = result.rows[0].id;
-
-        pool
-          .query("UPDATE quest SET quest_status = $1 WHERE id = $2", [
-            newQuestStatus,
-            questId,
-          ])
-          .then(() => {
-            console.log("Data updated successfully");
-            res.writeHead(200, { "Content-Type": "application/json" });
-            res.end(
-              JSON.stringify({ message: "Quest status updated successfully" })
-            );
-          })
-          .catch((error) => {
-            console.error("Error updating data:", error);
-            res.writeHead(500, { "Content-Type": "application/json" });
-            res.end(JSON.stringify({ error: "Internal Server Error" }));
-          });
-        // .finally(() => {
-        //   // Close the database pool
-        //   pool.end();
-        // });
-      }
-    );
+    pool
+      .query("UPDATE quest SET quest_status = $1 WHERE id = $2", [
+        newQuestStatus,
+        questId,
+      ])
+      .then(() => {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(
+          JSON.stringify({ message: "Quest status updated successfully" })
+        );
+      })
+      .catch((error) => {
+        console.error("Error updating data:", error);
+        res.writeHead(500, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ error: "Internal Server Error" }));
+      });
+    // .finally(() => {
+    //   // Close the database pool
+    //   pool.end();
+    // });
   } catch (error) {
     console.error("Error parsing quest data:", error);
     res.writeHead(400, { "Content-Type": "application/json" });
