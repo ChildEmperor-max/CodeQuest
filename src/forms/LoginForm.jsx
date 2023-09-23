c1930b9a10f3ab54e2006fa061e5e7ec3b9bd30e
Rogelio L. Haban Jr <rogeliohabanjr19@gmail.com>
2023-09-23 09:38:28 +0800

    added temporary files

---
+++ src/forms/LoginForm.jsx
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+
+const LoginForm = () => {
+  return <div>Login</div>;
+};
+
+export default LoginForm;
---
+++ src/forms/RegistrationForm.jsx
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+
+const RegistrationForm = () => {
+  return <div>Register</div>;
+};
+
+export default RegistrationForm;
