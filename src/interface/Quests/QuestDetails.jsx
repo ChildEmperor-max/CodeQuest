3eda78ffdf7b7d990321bd62e5a3e013fcb06f16
Rogelio L. Haban Jr <rogeliohabanjr19@gmail.com>
2023-09-28 16:37:41 +0800

    added flip animation for testing

--- src/interface/Quests/QuestDetails.jsx
+++ src/interface/Quests/QuestDetails.jsx
@@ -3,22 +3,46 @@ import CloseButtonModal from "../../components/CloseButtonModal";
 import QuestSideButton from "./QuestSideButton";
 
 const QuestDetails = ({ quest, isEditorOpen, onStart, onAbandon, onClose }) => {
+  const [playFlipAnimation, setPlayFlipAnimation] = useState(true);
+  const [isFirstRender, setIsFirstRender] = useState(true);
+
+  useEffect(() => {
+    if (isFirstRender) {
+      // Play initial animation
+      setTimeout(() => {
+        setIsFirstRender(false);
+      }, 800);
+    } else {
+      // Play fade animation when the quest id changes
+      setPlayFlipAnimation(true);
+      setTimeout(() => {
+        setPlayFlipAnimation(false);
+      }, 500);
+    }
+  }, [quest.id]);
+
   return (
-    <div className={`quest-details ${isEditorOpen ? "on-editor" : ""} `}>
-      <div className="quest-details-header">
-        <CloseButtonModal onClose={onClose} />
-      </div>
-      <div className="quest-details-content">
-        <p className="quest-details-title">{quest.quest_title}</p>
-        <p>{quest.quest_description}</p>
-        <p>{quest.npc_name}</p>
-      </div>
-      <div className="quest-details-buttons">
-        <QuestSideButton
-          quest={quest}
-          onStart={onStart}
-          onAbandon={onAbandon}
-        />
+    <div
+      className={`quest-details ${isEditorOpen ? "on-editor" : ""} ${
+        isFirstRender && "play-initial-animation"
+      }`}
+    >
+      <div className={`${playFlipAnimation ? "flip-from-bottom-to-top" : ""}`}>
+        <div className="quest-details-header">
+          <CloseButtonModal onClose={onClose} />
+        </div>
+        <div className="quest-details-content">
+          <p className="quest-details-title">{quest.quest_title}</p>
+          <p>{quest.quest_description}</p>
+          <p>{quest.npc_name}</p>
+        </div>
+        <div className="quest-details-buttons">
+          <QuestSideButton
+            quest={quest}
+            onStart={onStart}
+            onAbandon={onAbandon}
+          />
+        </div>
       </div>
     </div>
   );
