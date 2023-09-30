4e603fbdf0d8685fcdb2e46d03026c088d2205eb
ChildEmperor-max <moranojayson@gmail.com>
2023-09-30 12:32:33 +0800

    added router

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import "./styles/Quests.css";
@@ -19,6 +20,8 @@ import "./styles/AlbyInterface.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-  <App />
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
   // </React.StrictMode>
 );
