7e67eb74a9517f6aea4f043e6e59df46687d9cce
ChildEmperor-max <moranojayson@gmail.com>
2023-09-30 12:31:53 +0800

    use routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,113 +1,20 @@
 import React, { useEffect, useState } from "react";
-import InterfaceHandler from "./interface/InterfaceHandler";
-import SceneInit from "./lib/SceneInit";
-import * as THREE from "three";
-import Player from "./player/Player";
-import SampleNPC1 from "./npc/SampleNPC1";
-import SampleNPC2 from "./npc/SampleNPC2";
-import AlbyNPC from "./npc/AlbyNPC";
-import CameraController from "./lib/CameraControls";
+import { Routes, Route } from "react-router-dom";
+import LoginForm from "./forms/login/LoginForm";
+import SignupForm from "./forms/signup/SignupForm";
+import Game from "./Game";
+import LandingPage from "./forms/index/LandingPage";
 
 function App() {
-  const [antialiasValue, setAntialiasValue] = useState(false);
-  const [shadowMap, setShadowMap] = useState(false);
-  const [renderDistance, setRenderDistance] = useState(300);
-  const player = new Player();
-
-  const fov = 65;
-  const aspectRatio = window.innerWidth / window.innerHeight;
-  const cameraNear = 0.1;
-  const cameraFar = 1000;
-  const camera = new THREE.PerspectiveCamera(
-    fov,
-    aspectRatio,
-    cameraNear,
-    cameraFar
-  );
-  const cameraControls = new CameraController();
-
-  const sampleNPC1 = new SampleNPC1();
-  const sampleNPC2 = new SampleNPC2();
-  const albyNPC = new AlbyNPC();
-
-  const npcs = {
-    sampleNPC1: sampleNPC1,
-    sampleNPC2: sampleNPC2,
-    albyNPC: albyNPC,
-  };
-
-  const npcArray = [sampleNPC1, sampleNPC2, albyNPC];
-
-  useEffect(() => {
-    const canvas = document.getElementById("myThreeJsCanvas");
-    const renderer = new THREE.WebGLRenderer({
-      canvas,
-      antialias: antialiasValue,
-    });
-
-    const world = new SceneInit("myThreeJsCanvas", renderer);
-    world.initialize(player, npcs, camera, cameraControls);
-
-    const startAnimation = () => {
-      world.startAnimation();
-    };
-
-    const stopAnimation = () => {
-      world.stopAnimation();
-    };
-
-    // Handle page visibility change
-    const handleVisibilityChange = () => {
-      if (document.hidden) {
-        stopAnimation();
-      } else {
-        startAnimation();
-      }
-    };
-
-    // Add event listeners for page visibility change
-    document.addEventListener("visibilitychange", handleVisibilityChange);
-
-    startAnimation();
-
-    renderer.shadowMap.enabled = shadowMap;
-    renderer.shadowMap.type = THREE.PCFShadowMap;
-    // THREE.BasicShadowMap
-    // THREE.PCFShadowMap (default)
-    // THREE.PCFSoftShadowMap
-    // THREE.VSMShadowMap
-
-    return () => {
-      renderer.dispose();
-      // Remove event listeners for cleanup
-      document.removeEventListener("visibilitychange", handleVisibilityChange);
-    };
-  }, [antialiasValue, shadowMap]);
-
   return (
-    <div>
-      <InterfaceHandler
-        settings={{
-          antialias: {
-            antialiasValue: antialiasValue,
-            setAntialiasValue: setAntialiasValue,
-          },
-          shadowMap: {
-            shadowMapValue: shadowMap,
-            setShadowMapValue: setShadowMap,
-          },
-        }}
-        playerInstance={player}
-        npcInstances={npcArray}
-        cameraInstance={camera}
-        cameraControllerInstance={cameraControls}
-      />
-      <canvas
-        id="myThreeJsCanvas"
-        width={window.innerWidth}
-        height={window.innerHeight}
-      ></canvas>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/signup" element={<SignupForm />} />
+        <Route path="/game" element={<Game />} />
+      </Routes>
+    </>
   );
 }
 
