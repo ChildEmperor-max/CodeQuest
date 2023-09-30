a42d8fbf08373f5c3b491494351d27f73534c0e2
ChildEmperor-max <moranojayson@gmail.com>
2023-09-30 12:32:55 +0800

    modified port

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000, // Specify the port for the Vite development server
+    port: 3000,
+    // port: 8000, // Specify the port for the Vite development server
     proxy: {
       "/api": {
         target: "http://localhost:8000", // Proxy API requests to your server (change the URL to match your server setup)
