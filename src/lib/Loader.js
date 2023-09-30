c1211bcbe882df3483edc676d11b0aaaa21123ab
ChildEmperor-max <moranojayson@gmail.com>
2023-09-30 12:29:02 +0800

    modified loader manager

--- src/lib/Loader.js
+++ src/lib/Loader.js
@@ -1,29 +1,45 @@
 import * as THREE from "three";
 
-export default class Loader {
-  constructor(text) {
+export default class ManageLoader {
+  constructor(object, divElement, textElement) {
     this.loadingManager = new THREE.LoadingManager();
+    this.loadingState = {
+      started: "started",
+      onProgress: "onPrgress",
+      finished: "finished",
+    };
+    this.object = object;
 
-    this.loadingGameScreenDiv = document.getElementById("loading-game-screen");
-    this.loadingGameScreenText = document.getElementById(
-      "loading-game-screen-text"
-    );
+    this.loadingGameScreenDiv = divElement;
+    this.loadingGameScreenText = textElement;
 
     // Set up the loading manager callbacks
     this.loadingManager.onStart = () => {
       this.loadingGameScreenDiv.style.display = "block";
-      this.loadingGameScreenText.textContent = text;
+      this.loadingGameScreenText.textContent = object;
+      const event = new CustomEvent("loadingStarted", {
+        detail: 0,
+      });
+      document.dispatchEvent(event);
     };
 
     this.loadingManager.onProgress = (url, itemsLoaded, itemsTotal) => {
       const progress = (itemsLoaded / itemsTotal) * 100;
       const formattedProgress = progress.toFixed(0);
-      this.loadingGameScreenText.textContent = `${text} ${formattedProgress}%`;
+      this.loadingGameScreenText.textContent = `${object} ${formattedProgress}%`;
+      const event = new CustomEvent("gameAssetLoader", {
+        detail: formattedProgress,
+      });
+      document.dispatchEvent(event);
     };
 
     this.loadingManager.onLoad = () => {
       this.loadingGameScreenText.textContent = "";
       this.loadingGameScreenDiv.style.display = "none";
+      const event = new CustomEvent("loadingFinished", {
+        detail: 100,
+      });
+      document.dispatchEvent(event);
     };
   }
 
