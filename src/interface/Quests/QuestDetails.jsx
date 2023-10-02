6e7c61901cdb00d6cc433b62ec1ff04ebcf37b27
Rogelio L. Haban Jr <rogeliohabanjr19@gmail.com>
2023-10-02 20:53:58 +0800

    fixed quest details bug

--- src/interface/Quests/QuestDetails.jsx
+++ src/interface/Quests/QuestDetails.jsx
@@ -2,7 +2,13 @@ import React, { useState, useEffect } from "react";
 import CloseButtonModal from "../../components/CloseButtonModal";
 import QuestSideButton from "./QuestSideButton";
 
-const QuestDetails = ({ quest, isEditorOpen, onStart = null, onAbandon = null, onClose }) => {
+const QuestDetails = ({
+  quest,
+  isEditorOpen,
+  onStart = null,
+  onAbandon = null,
+  onClose,
+}) => {
   const [playFlipAnimation, setPlayFlipAnimation] = useState(true);
   const [isFirstRender, setIsFirstRender] = useState(true);
 
@@ -22,30 +28,37 @@ const QuestDetails = ({ quest, isEditorOpen, onStart = null, onAbandon = null, o
   }, [quest.id]);
 
   return (
-    <div
-      className={`quest-details ${isEditorOpen ? "on-editor" : ""} ${
-        isFirstRender && "play-initial-animation"
-      }`}
-    >
-      <div className={`${playFlipAnimation ? "flip-from-bottom-to-top" : ""}`}>
-        <div className="quest-details-header">
-          <CloseButtonModal onClose={onClose} />
+    <>
+      {quest ? (
+        <div
+          className={`quest-details ${isEditorOpen ? "on-editor" : ""} ${
+            isFirstRender && "play-initial-animation"
+          }`}
+        >
+          <div
+            className={`${playFlipAnimation ? "flip-from-bottom-to-top" : ""}`}
+          >
+            <div className="quest-details-header">
+              <CloseButtonModal onClose={onClose} />
+            </div>
+            <div className="quest-details-content">
+              <p className="quest-details-title">{quest.quest_title}</p>
+              <p>{quest.quest_description}</p>
+              <p>{quest.npc_name}</p>
+            </div>
+            <div className="quest-details-buttons">
+              {onStart && (
+                <QuestSideButton
+                  quest={quest}
+                  onStart={onStart}
+                  onAbandon={onAbandon}
+                />
+              )}
+            </div>
+          </div>
         </div>
-        <div className="quest-details-content">
-          <p className="quest-details-title">{quest.quest_title}</p>
-          <p>{quest.quest_description}</p>
-          <p>{quest.npc_name}</p>
-        </div>
-        <div className="quest-details-buttons">
-          {onStart && 
-          <QuestSideButton
-            quest={quest}
-            onStart={onStart}
-            onAbandon={onAbandon}
-          />}
-        </div>
-      </div>
-    </div>
+      ) : null}
+    </>
   );
 };
 
