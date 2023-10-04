260b1f44bedfc1fdb1c3d244d3bb4403d412287a
Rogelio L. Haban Jr <rogeliohabanjr19@gmail.com>
2023-10-04 14:40:29 +0800

    basic style for signup page

--- src/forms/signup/SignupForm.jsx
+++ src/forms/signup/SignupForm.jsx
@@ -1,11 +1,122 @@
-import React, { useState, useEffect } from "react";
-import "./SignupForm.css"
+import React, { useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import "./SignupForm.css";
 
 const SignupForm = () => {
-  useEffect(() => {
-    console.log("signup")
-  }, [])
-  return <div className="signup-form-container">Signup</div>;
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    gender: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/api/users/signup",
+        formData,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (response.status === 201) {
+        console.log("Signup successful:", response.data);
+        // Redirect to a success page or login page
+      } else {
+        console.log("Signup failed");
+      }
+    } catch (error) {
+      console.error("Error signing up:", error);
+    }
+  };
+
+  return (
+    <div className="signup-form-container">
+      <h2>Signup</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="firstName">First Name</label>
+          <input
+            type="text"
+            id="firstName"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="lastName">Last Name</label>
+          <input
+            type="text"
+            id="lastName"
+            name="lastName"
+            value={formData.lastName}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="gender">Gender</label>
+          <select
+            id="gender"
+            name="gender"
+            value={formData.gender}
+            onChange={handleChange}
+            required
+          >
+            <option value="">Select Gender</option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="other">Other</option>
+          </select>
+        </div>
+        <div className="form-group">
+          <button type="submit">Signup</button>
+          <Link to="/login">
+            <p>Already have an account?</p>
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default SignupForm;
