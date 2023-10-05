c95fafbe2c234bc924c96bc5daea551fa79cfd5c
Rogelio L. Haban Jr <rogeliohabanjr19@gmail.com>
2023-10-05 17:45:29 +0800

    organized

--- server/sql/viewHelpDialogById.sql
+++ server/sql/viewHelpDialogById.sql
@@ -1,4 +1,5 @@
-SELECT h.id, h.title, h.description, h.template, h.one_line_highlight, h.multi_line_highlight FROM help as h
+SELECT h.id, h.title, h.description, h.template, h.one_line_highlight, h.multi_line_highlight 
+FROM help as h
 LEFT JOIN dialog
 ON dialog.quest_hint_id = h.id
 WHERE h.id = $1
