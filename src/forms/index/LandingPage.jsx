0a312c15084b79e8b01308450dc7da09dab3efb6
Rogelio L. Haban Jr <rogeliohabanjr19@gmail.com>
2023-10-09 18:48:40 +0800

    new landing page

--- src/forms/index/LandingPage.jsx
+++ src/forms/index/LandingPage.jsx
@@ -1,20 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./LandingPage.css";
+import Main from "./components/Main";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const LandingPage = () => {
+  document.body.classList.add("overflow-auto");
   return (
-    <div className="landing-page-container">
-      <p>Welcome to CodeQuest!</p>
-      <div className="buttons-container">
-        <Link to="/login">
-          <button>Login</button>
-        </Link>
-        <Link to="/signup">
-          <button>Signup</button>
-        </Link>
-      </div>
-    </div>
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+    // <div className="landing-page-container">
+    //   <p>Welcome to CodeQuest!</p>
+    //   <div className="buttons-container">
+    //     <Link to="/login">
+    //       <button>Login</button>
+    //     </Link>
+    //     <Link to="/signup">
+    //       <button>Signup</button>
+    //     </Link>
+    //   </div>
+    // </div>
   );
 };
 
