cf86cd720cb5f3b08410d5ead471b7042fa85206
Rogelio L. Haban Jr <rogeliohabanjr19@gmail.com>
2023-11-05 12:46:45 +0800

    updated query

--- server/sql/npc/selectNpcByQuestId.sql
+++ server/sql/npc/selectNpcByQuestId.sql
@@ -1,5 +1,5 @@
-SELECT n.id, n.npc_name, n.quest_id, n.dialog_id
+SELECT n.id, n.npc_name
 FROM npc AS n
 LEFT JOIN player_quests AS p
-ON n.quest_id = p.quest_id
-WHERE p.quest_id = $1
+ON n.id = p.npc_id
+WHERE p.quest_id = $1 AND p.player_id = $2
