d1fd5b3b818ef3be60e298c4473045c55123aa5a
Rogelio L. Haban Jr <rogeliohabanjr19@gmail.com>
2023-12-03 20:31:39 +0800

    for setting the quests back to default

--- server/sql/resetToDefault.sql
+++ server/sql/resetToDefault.sql
@@ -1,7 +1,11 @@
 UPDATE player_quests
-SET quest_status = 'inactive', dialog_id = 1, quest_id = 3
-WHERE id = 2
+SET quest_status = 'inactive', dialog_id = 1, npc_id = 3
+WHERE id = 133
 
 UPDATE player_quests
-SET quest_status = 'locked', dialog_id = 8, quest_id = 4
-WHERE id = 1
+SET quest_status = 'locked', dialog_id = 8, npc_id = 4
+WHERE id = 134
+
+UPDATE player_quests
+SET quest_status = 'locked', dialog_id = 27, npc_id = 5
+WHERE id = 135
