6be04411c5831704b7242fdd7bc1455ec32e3787
Rogelio L. Haban Jr <rogeliohabanjr19@gmail.com>
2023-12-03 20:32:01 +0800

    updated default inventory value

--- server/sql/character/insertCharacterByPlayerid.sql
+++ server/sql/character/insertCharacterByPlayerid.sql
@@ -1,2 +1,2 @@
-INSERT into character (player_id, character_name, level, xp, character_bio)
-VALUES ($1, 'not set', 1, '{"current_xp": 0, "max_xp": 20}', 'not set');
+INSERT into character (player_id, character_name, level, xp, character_bio, inventory)
+VALUES ($1, $2, 1, '{"current_xp": 0, "max_xp": 20}', 'not set', '{}');
