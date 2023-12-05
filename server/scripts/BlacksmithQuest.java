7c833e078322a4a6b8f8f17f0c1a5faaf2fef281
ChildEmperor-max <moranojayson@gmail.com>
2023-12-05 21:11:29 +0800

    added script

---
+++ server/scripts/BlacksmithQuest.java
@@ -0,0 +1,9 @@
+public class BlacksmithQuest {
+    public static void main(String[] args) {
+        // Declare variable and assign it an initial value
+        int sharpness = 100; 
+
+        // Display "Kael sword sharpness: "
+        System.out.println("NPC sword sharpness: " + sharpness);
+    }
+}
---
+++ server/scripts/VeilOfTheAncients.java
@@ -0,0 +1,10 @@
+public class VeilOfTheAncients {
+    public static void main(String[] args) {
+        System.out.println("In the scrolls of code, an ancient secret sleeps,");
+        /*System.out.println("Between the lines, a tale of magic creeps.");
+        System.out.println("Whispers in variables, a mystical song,");
+        System.out.println("But one part veiled, in comments it belongs.");*/
+        System.out.println("A guardian of secrets, code's mysteries guards,");              
+        System.out.println("Unlocking wisdom, with coding rewards.");
+    }
+}
