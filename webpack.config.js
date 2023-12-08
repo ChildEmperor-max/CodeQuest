d0a051ff7fcc21c56e6d2503cb38438573f90a22
ChildEmperor-max <moranojayson@gmail.com>
2023-12-08 13:50:52 +0800

    try

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-  entry: "./src/world/LoadWorld.js", // Adjust the path to your main JavaScript file
+  entry: "./src/lib/SceneInit.js", // Adjust the path to your main JavaScript file
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
