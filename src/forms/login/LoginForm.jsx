4e78ade9e06279da6b9fdcdc13b4ab49d080ee79
ChildEmperor-max <moranojayson@gmail.com>
2023-12-10 14:37:39 +0800

    test

--- src/forms/login/LoginForm.jsx
+++ src/forms/login/LoginForm.jsx
@@ -37,7 +37,7 @@ const LoginForm = ({ darkMode }) => {
 
       const response = await axios.post(
         // "http://localhost:8000/api/users/login",
-        "/netlify/functions/login",
+        "/.netlify/functions/login",
         jsonData,
         {
           headers: {
