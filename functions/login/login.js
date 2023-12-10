368588bc51e6b91666a0416897b4e3fa31f898ce
ChildEmperor-max <moranojayson@gmail.com>
2023-12-10 17:19:05 +0800

    test

--- functions/login/login.js
+++ functions/login/login.js
@@ -1,41 +1,33 @@
+const { createClient } = require("@supabase/supabase-js");
+const db = require("../Models");
+const Player = db.players;
+
+const supabase = createClient(
+  "https://lijmzdfdpsabhpwqlfnh.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxpam16ZGZkcHNhYmhwd3FsZm5oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE1MDQzNzcsImV4cCI6MjAxNzA4MDM3N30.YJPUGL7jXFlf6SG_mE_k4cR9aVtnlUFNGqfVjnVb8ZM"
+);
+
 const handler = async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    //find a user by their email
-    const user = await Player.findOne({
-      where: {
-        email: email,
-      },
-    });
+    const { data, error } = await supabase
+      .from("players")
+      .select()
+      .eq("email", email);
 
-    //if user email is found, compare password with bcrypt
-    if (user) {
-      const isSame = await bcrypt.compare(password, user.password);
-
-      //if password is the same
-      //generate token with the user's id and the secretKey in the env file
-
-      if (isSame) {
-        let token = jwt.sign({ id: user.id }, process.env.secretKey, {
-          expiresIn: 1 * 24 * 60 * 60 * 1000,
-        });
-
-        //if password matches with the one in the database
-        //go ahead and generate a cookie for the user
-        res.cookie("jwt", token, { maxAge: 1 * 24 * 60 * 60, httpOnly: true });
-        console.log("user", JSON.stringify(user, null, 2));
-        console.log("LOGIN SUCCESSFULLY!!!");
-        //send user data
-        return res.status(201).send(user);
-      } else {
-        return res.status(401).send("Wrong password");
-      }
-    } else {
-      return res.status(401).send("Account does not exists");
+    if (error) {
+      return res.status(500).send(error.message);
     }
+
+    const user = data ? data[0] : null;
+
+    // Continue with your existing logic...
+
+    return res.status(201).send(user);
   } catch (error) {
     console.log(error);
+    return res.status(500).send("Internal Server Error");
   }
 };
 
