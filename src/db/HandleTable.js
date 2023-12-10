9860f73c5e9c8ce14a5448536d632ddc83aec0c9
ChildEmperor-max <moranojayson@gmail.com>
2023-12-11 00:28:19 +0800

    test

--- src/db/HandleTable.js
+++ src/db/HandleTable.js
@@ -717,7 +717,7 @@ export async function fetchPlayerQuests(id) {
   const { data, error } = await supabase
     .from("player_quests")
     .select(`quest.*, player_quests.*`)
-    .eq("player_id", id);
+    .eq("player_quests.player_id", id);
   if (data) {
     return data;
   }
