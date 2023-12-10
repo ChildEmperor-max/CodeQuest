61c617c62d5772e7200a4830ecd5c1fc16e97f93
ChildEmperor-max <moranojayson@gmail.com>
2023-12-10 13:04:36 +0800

    added script

--- server/scripts/WordWeaver.java
+++ server/scripts/WordWeaver.java
@@ -1,6 +1,18 @@
-public class script {
-    public static void main(String args[]) {
-        System.out.print("Nathan");
-        
+public class WordWeaver {
+    public static void main(String[] args) {
+        String adjective = "Amazing";
+        String noun = "adventure";
+        String sentence;
+
+        sentence = "Embark on an " + adjective + " " + noun + " in the world of coding.";
+
+        System.out.println(sentence);
+
+        int sentenceLength = sentence.length();
+        System.out.println("Sentence length: " + sentenceLength);
+
+        String adventurePart = sentence.substring(17, 26);
+        System.out.println("Adventure part: " + adventurePart);
     }
 }
+
