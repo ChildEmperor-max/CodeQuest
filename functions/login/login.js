c07f09aa772e52ec68545eac87295465a7c897af
ChildEmperor-max <moranojayson@gmail.com>
2023-12-10 17:41:55 +0800

    test

--- functions/login/login.js
+++ functions/login/login.js
@@ -23,12 +23,40 @@ const handler = async (event) => {
 
     const user = data ? data[0] : null;
 
-    // Continue with your existing logic...
+    if (user) {
+      const isSame = await bcrypt.compare(password, user.password);
 
-    return {
-      statusCode: 201,
-      body: JSON.stringify(user),
-    };
+      //if password is the same
+      //generate token with the user's id and the secretKey in the env file
+
+      if (isSame) {
+        let token = jwt.sign({ id: user.id }, process.env.secretKey, {
+          expiresIn: 1 * 24 * 60 * 60 * 1000,
+        });
+
+        //if password matches with the one in the database
+        //go ahead and generate a cookie for the user
+        res.cookie("jwt", token, { maxAge: 1 * 24 * 60 * 60, httpOnly: true });
+        console.log("user", JSON.stringify(user, null, 2));
+        console.log("LOGIN SUCCESSFULLY!!!");
+        //send user data
+
+        return {
+          statusCode: 201,
+          body: JSON.stringify(user),
+        };
+      } else {
+        return {
+          statusCode: 401,
+          body: JSON.stringify("Wrong password"),
+        };
+      }
+    } else {
+      return {
+        statusCode: 401,
+        body: JSON.stringify("Account does not exists"),
+      };
+    }
   } catch (error) {
     console.log(error);
     return {
