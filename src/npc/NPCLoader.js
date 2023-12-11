2799db0c8f4b3b86df144632361eadb87f15c685
Rogelio L. Haban Jr <rogeliohabanjr19@gmail.com>
2023-12-11 21:36:38 +0800

    try

--- src/npc/NPCLoader.js
+++ src/npc/NPCLoader.js
@@ -144,8 +144,8 @@ export default class NPCLoader extends Interactibles {
     async function fetchData(npcName) {
       try {
         this.npcData = await viewNpcData(npcName, this.playerId);
-        console.log("THIS NPC DATA: ", this.npcData);
-        if (this.npcData) {
+        if (this.npcData.npc_name) {
+          console.log("THIS NPC DATA: ", this.npcData);
           // if (this.npcData[0].quest_status !== "locked") {
           //   if (this.npcData[0].quest_status !== "completed") {
           if (this.npcData.quest_id) {
