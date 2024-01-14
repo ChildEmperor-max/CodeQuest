6b0409b46450a5a554858ca019fc6e2482d43298
Rogelio L. Haban Jr <rogeliohabanjr19@gmail.com>
2024-01-14 14:18:24 +0800

    adjusted styles

--- src/interface/ItemDisplay/Items/CodeCipherScroll.jsx
+++ src/interface/ItemDisplay/Items/CodeCipherScroll.jsx
@@ -9,13 +9,13 @@ const CodeCipherScroll = ({ quest, onClose }) => {
   return (
     <>
       <div className="quest-hint-container">
-        <p>{quest.quest_title}</p>
+        <button onClick={onClose}>Close</button>
+        <p className="quest-hint-title">{quest.quest_title}</p>
         <p>{quest.hint}</p>
         <p>
           Note: Remember this hint because you won't be able to see this hint
           again once you close it unless you have another item.
         </p>
-        <button onClick={onClose}>Close</button>
       </div>
     </>
   );
